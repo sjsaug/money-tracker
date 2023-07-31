9d201e6cafe916128eddac5fd713a44ae0e9ec82
sjsaug <sjsaug@gmail.com>
2023-07-31 03:09:59 -0400

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const Transaction = require('./models/transaction.js');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
@@ -8,7 +11,8 @@ app.get("/api/test", (req, res) => {
     res.json("test ok")
 });
 
-app.post("/api/transaction", (req, res) => {
+app.post("/api/transaction", async (req, res) => {
+    await mongoose.connect(process.env.MONGO_URL)
     const {name, datetime, description} = req.body;
     
     res.json(req.body);
--- api/models/transaction.js
+++ api/models/transaction.js
@@ -1,6 +1,11 @@
-import {Schema} from 'mongoose';
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
 const TransactionSchema = new Schema({
     name: {type: String, required: true},
-    description: {type: String, required: true},
     datetime: {type: Date, required: true},
+    description: {type: String, required: true},
 });
+
+const TransactionModel = model("Transaction", TransactionSchema);
+
+module.exports = TransactionModel;
